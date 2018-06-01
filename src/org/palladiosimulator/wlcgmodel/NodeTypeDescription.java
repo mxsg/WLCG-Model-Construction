c4637bef8f833a185ef96cec5ab31bfd0740c6dc
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-06-01 17:39:00 +0200

    Extend node type description class

--- src/org/palladiosimulator/wlcgmodel/NodeTypeDescription.java
+++ src/org/palladiosimulator/wlcgmodel/NodeTypeDescription.java
@@ -1,6 +1,21 @@
 package org.palladiosimulator.wlcgmodel;
 
 public class NodeTypeDescription {
-	private int count;
-	private String typeName = null;
+	
+	private int count = 0;
+	private String nodeName = null;
+	private double computingRate = 0;
+	private int jobSlots = 0;
+	private int coreCount = 0;
+	
+	public NodeTypeDescription() {}
+
+	public NodeTypeDescription(int count, String nodeName, double computingRate, int jobSlots, int coreCount) {
+		super();
+		this.count = count;
+		this.nodeName = nodeName;
+		this.computingRate = computingRate;
+		this.jobSlots = jobSlots;
+		this.coreCount = coreCount;
+	}
 }
