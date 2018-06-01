9f09da1de67ce4b3f3b16b1ddd2dd4fab7a4aa41
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-06-01 17:40:14 +0200

    Add skeleton for JSON import class

---
+++ src/org/palladiosimulator/wlcgmodel/ParameterJSONImportHelper.java
@@ -0,0 +1,15 @@
+package org.palladiosimulator.wlcgmodel;
+
+import java.util.List;
+import org.eclipse.emf.common.util.URI;
+
+public class ParameterJSONImportHelper {
+	
+	public static List<NodeTypeDescription> readNodeTypes(URI jsonPath) {
+		return null;
+	}
+	
+	public static List<JobDescription> readJobTypes(URI jsonPath) {
+		return null;
+	}
+}
