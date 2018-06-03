22b622abb56950b5d637a34a4dc2f6348d9ff116
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-06-03 16:07:14 +0200

    Add json import helper functionality

--- org.palladiosimulator.wlcgmodel/src/org/palladiosimulator/wlcgmodel/ParameterJSONImportHelper.java
+++ org.palladiosimulator.wlcgmodel/src/org/palladiosimulator/wlcgmodel/ParameterJSONImportHelper.java
@@ -1,15 +1,36 @@
 package org.palladiosimulator.wlcgmodel;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
-import org.eclipse.emf.common.util.URI;
+
+import com.google.gson.Gson;
+
 
 public class ParameterJSONImportHelper {
 	
-	public static List<NodeTypeDescription> readNodeTypes(URI jsonPath) {
-		return null;
+	public static List<NodeTypeDescription> readNodeTypes(File jsonFile) throws FileNotFoundException, IOException {
+		
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(jsonFile));
+        Gson gson = new Gson();
+        
+        NodeTypeDescription[] nodesArray = gson.fromJson(bufferedReader, NodeTypeDescription[].class);
+		
+		bufferedReader.close();
+		return Arrays.asList(nodesArray);
 	}
 	
-	public static List<JobDescription> readJobTypes(URI jsonPath) {
-		return null;
+	public static List<JobTypeDescription> readJobTypes(File jsonFile) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(jsonFile));
+        Gson gson = new Gson();
+        
+        JobTypeDescription[] nodesArray = gson.fromJson(bufferedReader, JobTypeDescription[].class);
+		
+		bufferedReader.close();
+		return Arrays.asList(nodesArray);
 	}
 }
