1e6d01dc72809290846f87d5b262d963f19e2279
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-06-05 21:31:17 +0200

    Restructure JSON parsing helper class
    - Throw Exception on bad JSON Syntax for imported data
    - Use InputStreams instead of simple Readers

--- org.palladiosimulator.wlcgmodel/src/org/palladiosimulator/wlcgmodel/ParameterJSONImportHelper.java
+++ org.palladiosimulator.wlcgmodel/src/org/palladiosimulator/wlcgmodel/ParameterJSONImportHelper.java
@@ -1,36 +1,48 @@
 package org.palladiosimulator.wlcgmodel;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.Arrays;
 import java.util.List;
 
 import com.google.gson.Gson;
-
+import com.google.gson.JsonSyntaxException;
 
 public class ParameterJSONImportHelper {
-	
-	public static List<NodeTypeDescription> readNodeTypes(File jsonFile) throws FileNotFoundException, IOException {
-		
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(jsonFile));
+
+    // TODO Consolidate methods in a more generic way
+
+    public static List<NodeTypeDescription> readNodeTypes(File jsonFile) throws IOException, JsonSyntaxException {
+
+        List<NodeTypeDescription> result = null;
+
+        InputStream stream = new FileInputStream(jsonFile);
+        Reader reader = new InputStreamReader(stream);
+
         Gson gson = new Gson();
-        
-        NodeTypeDescription[] nodesArray = gson.fromJson(bufferedReader, NodeTypeDescription[].class);
-		
-		bufferedReader.close();
-		return Arrays.asList(nodesArray);
-	}
-	
-	public static List<JobTypeDescription> readJobTypes(File jsonFile) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new FileReader(jsonFile));
+
+        result = Arrays.asList(gson.fromJson(reader, NodeTypeDescription[].class));
+
+        reader.close();
+        return result;
+    }
+
+    public static List<JobTypeDescription> readJobTypes(File jsonFile) throws IOException, JsonSyntaxException {
+
+        List<JobTypeDescription> result = null;
+
+        InputStream stream = new FileInputStream(jsonFile);
+        Reader reader = new InputStreamReader(stream);
+
         Gson gson = new Gson();
-        
-        JobTypeDescription[] nodesArray = gson.fromJson(bufferedReader, JobTypeDescription[].class);
-		
-		bufferedReader.close();
-		return Arrays.asList(nodesArray);
-	}
+
+        result = Arrays.asList(gson.fromJson(reader, JobTypeDescription[].class));
+
+        reader.close();
+        return result;
+    }
 }
