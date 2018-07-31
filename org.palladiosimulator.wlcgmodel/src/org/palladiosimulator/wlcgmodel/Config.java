6e9c35a194df0dfdb6827f1b17cf32adac4373c5
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-07-31 19:37:12 +0200

    Document Configuration class

--- org.palladiosimulator.wlcgmodel/src/org/palladiosimulator/wlcgmodel/Config.java
+++ org.palladiosimulator.wlcgmodel/src/org/palladiosimulator/wlcgmodel/Config.java
@@ -1,18 +1,28 @@
 package org.palladiosimulator.wlcgmodel;
 
+/**
+ * This class contains project-wide configuration attributes.
+ *
+ * @author Maximilian Stemmer-Grabow
+ *
+ */
 public class Config {
-    
+
+
+    /** The platform-relative directory that hold the blueprint simulation models. */
     public static final String MODEL_BLUEPRINTS_DIRECTORY = "platform:/plugin/org.palladiosimulator.wlcgmodel/blueprint-wlcg";
-    
-    // Default values for the parameter files
-    // TODO Make this configurable or selectable in the UI
+
+    // TODO Future work: Make this configurable or selectable in the UI
+
+    /** Default value for the node description file to be used to calibrate the model. */
     public static final String NODE_DESCRIPTION_FILENAME = "nodes.json";
+
+    /** Default value for the job type description file to be used to calibrate the model. */
     public static final String JOB_DESCRIPTION_FILENAME = "jobs.json";
-    
+
     /**
      * Do not allow instantiation of utility class.
      */
     private Config() {
     }
-
 }
