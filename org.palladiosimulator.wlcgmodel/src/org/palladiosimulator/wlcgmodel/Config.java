b3bbe6850a4e501e7e5b1dbb41235eb89a3aa047
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-08-01 19:12:45 +0200

    Extract more static fields into Config class

--- org.palladiosimulator.wlcgmodel/src/org/palladiosimulator/wlcgmodel/Config.java
+++ org.palladiosimulator.wlcgmodel/src/org/palladiosimulator/wlcgmodel/Config.java
@@ -1,5 +1,7 @@
 package org.palladiosimulator.wlcgmodel;
 
+import org.eclipse.emf.common.util.URI;
+
 /**
  * This class contains project-wide configuration attributes.
  *
@@ -8,9 +10,17 @@ package org.palladiosimulator.wlcgmodel;
  */
 public final class Config {
 
+    /** The plugin ID */
+    public static final String PLUGIN_ID = "org.palladiosimulator.wlcgmodel";
 
     /** The platform-relative directory that hold the blueprint simulation models. */
     public static final String MODEL_BLUEPRINTS_DIRECTORY = "platform:/plugin/org.palladiosimulator.wlcgmodel/blueprint-wlcg";
+
+    /** The path to the simulation model blueprint. */
+    public static final URI MODEL_BLUEPRINT_URI = URI.createURI(MODEL_BLUEPRINTS_DIRECTORY);
+
+    /** Path to the directory containing the blueprint model files. */
+    public static final String MODEL_PARAMETER_FOLDER = "platform:/plugin/org.palladiosimulator.wlcgmodel/parameters";
 
     // TODO Future work: Make this configurable or selectable in the UI
 
@@ -18,7 +28,7 @@ public final class Config {
     public static final String NODE_DESCRIPTION_FILENAME = "nodes.json";
 
     /** Default value for the job type description file to be used to calibrate the model. */
-    public static final String JOB_DESCRIPTION_FILENAME = "jobs.json";
+    public static final String JOBS_DESCRIPTION_FILENAME = "jobs.json";
 
     /**
      * Do not allow instantiation of utility class.
