cc5f741a84b8b852b8bef7007bc56028af72e2b7
Maximilian Stemmer-Grabow <mail@mxsg.de>
2018-08-01 15:05:37 +0200

    Add Javadoc documentation to job and node type description classes

--- org.palladiosimulator.wlcgmodel/src/org/palladiosimulator/wlcgmodel/JobTypeDescription.java
+++ org.palladiosimulator.wlcgmodel/src/org/palladiosimulator/wlcgmodel/JobTypeDescription.java
@@ -1,8 +1,8 @@
 package org.palladiosimulator.wlcgmodel;
 
 /**
- * Instances of this class contain the parameters required to describe the properties
- * of a type of job to be included in the simulation model.
+ * Instances of this class contain the parameters required to describe the properties of a type of
+ * job to be included in the simulation model.
  *
  * @author Maximilian Stemmer-Grabow
  *
@@ -18,75 +18,134 @@ public class JobTypeDescription {
     private String ioTimeRatioStoEx = null;
     private int schedulingDelay = 12000;
 
+    /**
+     * Construct a new job type description instance
+     */
     public JobTypeDescription() {
     }
 
+    /**
+     * @return the typeName
+     */
     public String getTypeName() {
         return typeName;
     }
 
+    /**
+     * @param typeName
+     *            the typeName to set
+     */
     public void setTypeName(String typeName) {
         this.typeName = typeName;
     }
 
+    /**
+     * @return the interarrivalStoEx
+     */
     public String getInterarrivalStoEx() {
         return interarrivalStoEx;
     }
 
+    /**
+     * @param interarrivalStoEx
+     *            the interarrivalStoEx to set
+     */
     public void setInterarrivalStoEx(String interarrivalStoEx) {
         this.interarrivalStoEx = interarrivalStoEx;
     }
 
+    /**
+     * @return the cpuDemandStoEx
+     */
     public String getCpuDemandStoEx() {
         return cpuDemandStoEx;
     }
 
+    /**
+     * @param cpuDemandStoEx
+     *            the cpuDemandStoEx to set
+     */
     public void setCpuDemandStoEx(String cpuDemandStoEx) {
         this.cpuDemandStoEx = cpuDemandStoEx;
     }
 
+    /**
+     * @return the ioTimeStoEx
+     */
     public String getIoTimeStoEx() {
         return ioTimeStoEx;
     }
 
+    /**
+     * @param ioTimeStoEx
+     *            the ioTimeStoEx to set
+     */
     public void setIoTimeStoEx(String ioTimeStoEx) {
         this.ioTimeStoEx = ioTimeStoEx;
     }
 
+    /**
+     * @return the requiredJobslotsStoEx
+     */
     public String getRequiredJobslotsStoEx() {
         return requiredJobslotsStoEx;
     }
 
+    /**
+     * @param requiredJobslotsStoEx
+     *            the requiredJobslotsStoEx to set
+     */
     public void setRequiredJobslotsStoEx(String requiredJobslotsStoEx) {
         this.requiredJobslotsStoEx = requiredJobslotsStoEx;
     }
 
+    /**
+     * @return the relativeFrequency
+     */
     public double getRelativeFrequency() {
         return relativeFrequency;
     }
 
+    /**
+     * @param relativeFrequency
+     *            the relativeFrequency to set
+     */
     public void setRelativeFrequency(double relativeFrequency) {
         this.relativeFrequency = relativeFrequency;
     }
 
+    /**
+     * @return the ioTimeRatioStoEx
+     */
     public String getIoTimeRatioStoEx() {
         return ioTimeRatioStoEx;
     }
 
+    /**
+     * @param ioTimeRatioStoEx
+     *            the ioTimeRatioStoEx to set
+     */
     public void setIoTimeRatioStoEx(String ioTimeRatioStoEx) {
         this.ioTimeRatioStoEx = ioTimeRatioStoEx;
     }
 
+    /**
+     * @return the schedulingDelay
+     */
     public int getSchedulingDelay() {
         return schedulingDelay;
     }
 
+    /**
+     * @param schedulingDelay
+     *            the schedulingDelay to set
+     */
     public void setSchedulingDelay(int schedulingDelay) {
         this.schedulingDelay = schedulingDelay;
     }
 
     @Override
     public String toString() {
-        return "Job Type";
+        return "Job Type:" + this.typeName;
     }
 }
--- org.palladiosimulator.wlcgmodel/src/org/palladiosimulator/wlcgmodel/NodeTypeDescription.java
+++ org.palladiosimulator.wlcgmodel/src/org/palladiosimulator/wlcgmodel/NodeTypeDescription.java
@@ -1,5 +1,12 @@
 package org.palladiosimulator.wlcgmodel;
 
+/**
+ * Instances of this class contain the parameters required to describe the properties of a type of
+ * node to be included in the resource environment of the simulation model.
+ *
+ * @author Maximilian Stemmer-Grabow
+ *
+ */
 public class NodeTypeDescription {
 
     private String name = null;
@@ -8,51 +15,89 @@ public class NodeTypeDescription {
     private int cores = 0;
     private int nodeCount = 0;
 
+    /**
+     * Construct a new job type description instance
+     */
     public NodeTypeDescription() {
     }
 
+    /**
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * @param name
+     *            the name to set
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * @return the computingRate
+     */
     public double getComputingRate() {
         return computingRate;
     }
 
+    /**
+     * @param computingRate
+     *            the computingRate to set
+     */
     public void setComputingRate(double computingRate) {
         this.computingRate = computingRate;
     }
 
+    /**
+     * @return the jobslots
+     */
     public int getJobslots() {
         return jobslots;
     }
 
+    /**
+     * @param jobslots
+     *            the jobslots to set
+     */
     public void setJobslots(int jobslots) {
         this.jobslots = jobslots;
     }
 
+    /**
+     * @return the cores
+     */
     public int getCores() {
         return cores;
     }
 
+    /**
+     * @param cores
+     *            the cores to set
+     */
     public void setCores(int cores) {
         this.cores = cores;
     }
 
+    /**
+     * @return the nodeCount
+     */
     public int getNodeCount() {
         return nodeCount;
     }
 
+    /**
+     * @param nodeCount
+     *            the nodeCount to set
+     */
     public void setNodeCount(int nodeCount) {
         this.nodeCount = nodeCount;
     }
 
     @Override
     public String toString() {
-        return "Name: " + name + ", " + cores + " cores," + jobslots + " jobslots," + nodeCount + " machines";
+        return "Name: " + this.name + ", " + this.cores + " cores," + this.jobslots + " jobslots," + this.nodeCount + " machines";
     }
 }
